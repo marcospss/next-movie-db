ef7ce1722139d12e9fed40b2c301d47e2bf20e86
marcospss <marcospss@gmail.com>
2020-04-13 19:09:00 -0300

    Configuração global

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     roots: ['<rootDir>'],
     moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
-    setupFiles: ['<rootDir>/config/setup.js'],
+    setupFiles: ['<rootDir>/settings/enzymeSetup.js'],
     testPathIgnorePatterns: ['<rootDir>[/\\\\](build|docs|node_modules|.next)[/\\\\]'],
     transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
     testEnvironment: 'jsdom',
