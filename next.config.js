a732c0e45f1d697547dbb6498f2d5586e072c3a6
marcospss <marcospss@gmail.com>
2020-04-14 19:22:47 -0300

    Alias para imports absolutos

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
+  webpack(config, options) {
+    config.resolve.alias['components'] = path.join(__dirname, 'components');
+    config.resolve.alias['fetch-data'] = path.join(__dirname, 'fetch-data');
+    config.resolve.alias['models'] = path.join(__dirname, 'models');
+    config.resolve.alias['services'] = path.join(__dirname, 'services');
+    config.resolve.alias['settings'] = path.join(__dirname, 'settings');
+    return config;
+  },
   env: {
     APY_KEY: process.env.APY_KEY,
   },
